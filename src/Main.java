7f348d7cb0aa69d3af1534d90da2655e4cc32434
danielgibran18tj <danielgibran18tj@hotmail.com>
2023-04-01 02:25:31 -0500

    creada la estructura CRUD del proyecto

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,37 @@
 import java.sql.Connection;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Conexion conexion = new Conexion();
-        try(Connection cnx= conexion.get_connection()){
 
-        }catch(Exception e){
-            System.out.println(e);
-        }
+        Scanner sc = new Scanner(System.in);
+        int opcion=0;
+        do{
+            System.out.println("----------------------------");
+            System.out.println("Aplicacion de mensajes");
+            System.out.println("1. crear mensaje");
+            System.out.println("2. listar mensajes");
+            System.out.println("3. editar mensajes");
+            System.out.println("4. eliminar mensajes");
+            System.out.println("5. salir");
+            //leemos la opcion del usuario
+            opcion = sc.nextInt();
+            switch (opcion){
+                case 1:
+                    MensajesService.crear_mensaje();
+                    break;
+                case 2:
+                    MensajesService.listar_mensaje();
+                    break;
+                case 3:
+                    MensajesService.editar_mensaje();
+                    break;
+                case 4:
+                    MensajesService.borrar_mensaje();
+                    break;
+                default:
+                    break;
+            }
+        }while(opcion != 5);
+
     }}
