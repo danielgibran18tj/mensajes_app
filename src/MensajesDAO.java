de97a5cd69be75ee78fccf3bf0833eb94045e05a
danielgibran18tj <danielgibran18tj@hotmail.com>
2023-04-01 15:20:02 -0500

    completado el codigo para la insercion da datos (CRUD), operativo

--- src/MensajesDAO.java
+++ src/MensajesDAO.java
@@ -1,6 +1,25 @@
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
 public class MensajesDAO {
     public static void crear_Mensaje_DB(Mensajes mensaje) {
-
+        Conexion db_connect = new Conexion();
+        try(Connection conexion = db_connect.get_connection()){
+            PreparedStatement ps = null;        // crear insercion de datos
+            try{
+                String query = "INSERT INTO mensajes (mensaje, autor_mensaje) VALUES (?,?)";
+                ps = conexion.prepareStatement(query);
+                ps.setString(1, mensaje.getMensaje());
+                ps.setString(2, mensaje.getAutor_mensaje());
+                ps.executeUpdate();
+                System.out.println("mensaje creado!!");
+            }catch(SQLException ex){
+                System.out.println("ex");
+            }
+        }catch (SQLException e){
+            System.out.println(e);
+        }
     }
     public static void leer_mensajesDB(){
 
--- src/MensajesService.java
+++ src/MensajesService.java
@@ -1,6 +1,18 @@
+import java.util.Scanner;
+
 public class MensajesService {
     public static void crear_mensaje(){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("escribe tu mensaje");
+        String mensaje = sc.nextLine();
 
+        System.out.println("tu nombre:");
+        String nombre = sc.nextLine();
+
+        Mensajes registro = new Mensajes();
+        registro.setMensaje(mensaje);
+        registro.setAutor_mensaje(nombre);
+        MensajesDAO.crear_Mensaje_DB(registro);
     }
     public static void listar_mensaje(){
 
