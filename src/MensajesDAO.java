e1524f91d88dcd1f2a48a4acb0b0c01902606447
danielgibran18tj <danielgibran18tj@hotmail.com>
2023-04-01 02:28:26 -0500

    creada la estructura CRUD del proyecto, ya includo los 3 Mensajes.class

---
+++ src/MensajesDAO.java
@@ -0,0 +1,15 @@
+public class MensajesDAO {
+    public static void crear_Mensaje_DB(Mensajes mensaje) {
+
+    }
+    public static void leer_mensajesDB(){
+
+    }
+    public static void borrar_mensaje(int id_mensaje){
+
+    }
+    public static void actualizar_mensajeDB(Mensajes mensaje){
+
+    }
+
+}
---
+++ src/MensajesService.java
@@ -0,0 +1,14 @@
+public class MensajesService {
+    public static void crear_mensaje(){
+
+    }
+    public static void listar_mensaje(){
+
+    }
+    public static void borrar_mensaje(){
+
+    }
+    public static void editar_mensaje(){
+
+    }
+}
